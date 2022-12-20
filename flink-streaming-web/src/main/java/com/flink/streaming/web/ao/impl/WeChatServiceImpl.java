fe622c6994e5729d824939eb96d1ea906ba4951c
seven.si <Seven.Si@gtechdigi.com>
2022-12-20 18:01:06 +0800

    1.15.1 增加企业微信报警

--- flink-streaming-web/src/main/java/com/flink/streaming/web/ao/impl/WeChatServiceImpl.java
+++ flink-streaming-web/src/main/java/com/flink/streaming/web/ao/impl/WeChatServiceImpl.java
@@ -45,23 +45,23 @@ public class WeChatServiceImpl implements WeChatService {
             DeployModeEnum deployModeEnum = jobConfigDTO.getDeployModeEnum();
             String flinkRunConfig = jobConfigDTO.getFlinkRunConfig();
             String customArgs = jobConfigDTO.getCustomArgs();
-            if(StringUtils.isBlank(jobConfigDTO.getCustomArgs())){
+            if (StringUtils.isBlank(jobConfigDTO.getCustomArgs())) {
                 customArgs = "";
             }
-            if(StringUtils.isBlank(jobConfigDTO.getFlinkRunConfig())){
+            if (StringUtils.isBlank(jobConfigDTO.getFlinkRunConfig())) {
                 flinkRunConfig = "";
             }
             log.warn("\ncontent: {} \n 原因: {} \n ConfigID: {} \n jobID: {} \n 任务名称: {} \n 运行模式: {} \n 运行参数: {} \n 自定义参数: {} \n ",
                     content, cause, jobConfigId, jobId, jobName, deployModeEnum, flinkRunConfig, customArgs);
 
-            String failLog = "\ncontent: " + content + "\n" +
-                    "ConfigID: " + jobConfigId + "\n" +
-                    "原因:" + cause + "\n" +
-                    "jobID: " + jobId + "\n" +
-                    "任务名称: " + jobName + "\n" +
-                    "运行模式: " + deployModeEnum + "\n" +
-                    "运行参数: " + flinkRunConfig + "\n" +
-                    "自定义参数: " + customArgs + "\n";
+            String failLog = "\ncontent: " + content + "\n"
+                    + "ConfigID: " + jobConfigId + "\n"
+                    + "原因:" + cause + "\n"
+                    + "jobID: " + jobId + "\n"
+                    + "任务名称: " + jobName + "\n"
+                    + "运行模式: " + deployModeEnum + "\n"
+                    + "运行参数: " + flinkRunConfig + "\n"
+                    + "自定义参数: " + customArgs + "\n";
             String weChatUrl = systemConfigService.getSystemConfigByKey(SysConfigEnum.ENTERPRISEWECHAT_ALARM_URL.getKey());
             weChatAlarm.send(weChatUrl, failLog);
 
